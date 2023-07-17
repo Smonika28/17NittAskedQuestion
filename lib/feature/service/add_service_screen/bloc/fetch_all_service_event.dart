b6a4159ff59c58792f87b0b578d519b3f537af7b
Smonika28 <135808069+Smonika28@users.noreply.github.com>
2023-07-17 10:12:55 +0530

    Add files via upload

---
+++ lib/feature/service/add_service_screen/bloc/fetch_all_service_event.dart
@@ -0,0 +1,53 @@
+// part of 'fetch_all_service_bloc.dart';
+//
+//
+// abstract class FetchAllServiceEvent {}
+// class FetchAllServiceEvents extends FetchAllServiceEvent{}
+//
+//
+//
+//
+//
+// abstract class FetchAllFieldEvent {}
+// class ServiceDetailsFieldEvent extends FetchAllFieldEvent{
+//   String serviceID;
+//   ServiceDetailsFieldEvent({required this.serviceID});
+// }
+//
+//
+//
+
+part of 'fetch_all_service_bloc.dart';
+
+abstract class FetchAllServiceEvent {}
+class FetchAllServiceEvents extends FetchAllServiceEvent {}
+
+
+
+abstract class FetchAllFieldEvent {}
+
+class ServiceDetailsFieldEvent extends FetchAllFieldEvent {
+  String serviceID;
+  ServiceDetailsFieldEvent({required this.serviceID});
+}
+
+
+
+// class AddAllServiceEvents extends FetchAllFieldEvent {}
+//
+// class ServiceDetailFieldEvent extends AddAllServiceEvent {
+//   String serviceID;
+//
+//   ServiceDetailFieldEvent({required this.serviceID});
+// }
+
+class AddServiceEvent extends FetchAllFieldEvent {
+  int serviceID;
+   String offerId;
+  int offerPrice;
+
+  AddServiceEvent(
+      {required this.serviceID,
+      required this.offerId,
+      required this.offerPrice});
+}
